a933a96e492d02e89b6c79d586c9f4f39401cb82
Hennadii_Mariiev <marieff1990@gmail.com>
2022-02-01 09:56:54 +0200

    fix currentMonth issue

--- src/components/CodeList/CodeList.jsx
+++ src/components/CodeList/CodeList.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect, useCallback, useMemo } from "react";
-import { Button, Row, Col, Container, Spinner, Form } from "react-bootstrap";
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import { Button, Row, Col, Container, Spinner, Form } from 'react-bootstrap';
 
-import api from "../../api/axios";
-import { CodeTable } from "./CodeTable";
-import { toastMessage } from "./../../helpers/toast.helper";
-import styles from "./styles.module.scss";
+import api from '../../api/axios';
+import { CodeTable } from './CodeTable';
+import { toastMessage } from './../../helpers/toast.helper';
+import styles from './styles.module.scss';
 
 export function CodeList({ onCodeChange, isFetching }) {
   const [data, setData] = useState({ result: [], total: 0 });
@@ -12,26 +12,24 @@ export function CodeList({ onCodeChange, isFetching }) {
   const [isCurrentMonth, setIsCurrentMonth] = useState(false);
 
   useEffect(() => {
-    const isCurMonthFromLocStor = localStorage.getItem("is_current_month");
+    const isCurMonthFromLocStor = localStorage.getItem('is_current_month');
     if (isCurMonthFromLocStor) {
-      setIsCurrentMonth(isCurMonthFromLocStor === "true" ? true : false);
+      setIsCurrentMonth(isCurMonthFromLocStor === 'true' ? true : false);
     } else {
-      localStorage.setItem("is_current_month", "false");
+      localStorage.setItem('is_current_month', 'false');
     }
   }, []);
 
   useEffect(() => {
-    localStorage.setItem("is_current_month", isCurrentMonth);
-    document.querySelector("#switch-month").checked = isCurrentMonth;
+    localStorage.setItem('is_current_month', isCurrentMonth);
+    document.querySelector('#switch-month').checked = isCurrentMonth;
   }, [isCurrentMonth]);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const resAll = await api.get(
-          `/interventions?is_current_month=${isCurrentMonth}`
-        );
-        const resLastFive = await api.get("/interventions/last_five");
+        const resAll = await api.get(`/interventions?is_current_month=${isCurrentMonth}`);
+        const resLastFive = await api.get('/interventions/last_five');
 
         if (resAll.status === 200) {
           const { result, total } = resAll.data.data;
@@ -43,7 +41,7 @@ export function CodeList({ onCodeChange, isFetching }) {
           setLastFive(interventions);
         }
       } catch (error) {
-        toastMessage("error", `Ошибка: ${error.message}`);
+        toastMessage('error', `Ошибка: ${error.message}`);
       } finally {
         onCodeChange(false);
       }
@@ -71,18 +69,14 @@ export function CodeList({ onCodeChange, isFetching }) {
   }, []);
 
   const resultTable = useMemo(() => {
-    const currentMonth = new Date().getMonth();
-
     return data.result
       .map((elem, index) => (
         <tbody key={index}>
           <tr rowSpan={elem.codes.length || 1}>
             <td className={styles.month}>{index + 1}</td>
             <td className={styles.month}>{elem.month.toUpperCase()}</td>
-            <td className={styles.td}>
-              {prepareCodeItems(elem.codes, "code")}
-            </td>
-            <td>{prepareCodeItems(elem.codes, "amount")}</td>
+            <td className={styles.td}>{prepareCodeItems(elem.codes, 'code')}</td>
+            <td>{prepareCodeItems(elem.codes, 'amount')}</td>
           </tr>
           <tr>
             <td></td>
@@ -96,17 +90,17 @@ export function CodeList({ onCodeChange, isFetching }) {
           </tr>
         </tbody>
       ))
-      .filter((el, index) => (isCurrentMonth ? index === currentMonth : el));
+      .filter((el, index) => (isCurrentMonth ? index === 0 : el));
   }, [data.result, prepareCodeItems, isCurrentMonth]);
 
   const prepareDate = (dateStr) => {
     const newDate = new Date(dateStr);
-    return newDate.toLocaleString("ru", {
-      day: "numeric",
-      month: "short",
-      year: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
+    return newDate.toLocaleString('ru', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
     });
   };
 
@@ -120,10 +114,7 @@ export function CodeList({ onCodeChange, isFetching }) {
     });
   };
 
-  const codeTable = useMemo(
-    () => <CodeTable data={resultTable} />,
-    [resultTable]
-  );
+  const codeTable = useMemo(() => <CodeTable data={resultTable} />, [resultTable]);
 
   return (
     <Container>
@@ -135,17 +126,15 @@ export function CodeList({ onCodeChange, isFetching }) {
       <Row className="mb-3">
         <Col sm="12" md="5">
           <h5>Последние добавленные</h5>
-          <ul className={styles.last}>
-            {!isFetching && prepareLastFiveList()}
-          </ul>
+          <ul className={styles.last}>{!isFetching && prepareLastFiveList()}</ul>
         </Col>
         <Col
           sm="6"
           md="4"
           style={{
-            display: "flex",
-            alignItems: "flex-start",
-            marginBottom: "1rem",
+            display: 'flex',
+            alignItems: 'flex-start',
+            marginBottom: '1rem',
           }}
         >
           <Form className={styles.switch_month}>
@@ -171,10 +160,10 @@ export function CodeList({ onCodeChange, isFetching }) {
           animation="border"
           variant="primary"
           style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            margin: "2rem auto",
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            margin: '2rem auto',
           }}
         />
       ) : (
